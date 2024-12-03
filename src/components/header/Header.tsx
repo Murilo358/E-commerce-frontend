d2ea341831be314cc8cd64cbdface17641fd00d5
Murilo358 <murilobarbosa358@gmail.com>
2024-12-02 22:56:00 -0300

    first commit

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,47 @@
+import { FC } from 'react';
+import './Header.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faShoppingCart, faUser } from '@fortawesome/free-solid-svg-icons';
+
+
+const Header: FC = () => {
+  return (
+    <header className='background'>
+        <div className="container header">
+    <div className="header__logo">
+        <a href="/">
+          <img src="logo.png" alt="Logo da loja" />
+        </a>
+      </div>
+
+      <div className="header__search">
+        <input
+          type="text"
+          placeholder="Buscar produtos..."
+        />
+        <button>
+        <FontAwesomeIcon icon={faSearch} />
+        </button>
+      </div>
+      <nav className="header__nav">
+          <ul>
+            <li>Home</li>
+            <li>Produtos</li>
+            <li>Contato</li>
+          </ul>
+        </nav>
+      <div className="header__icons">
+        <a href="/carrinho">
+        <FontAwesomeIcon icon={faShoppingCart}/>
+          <span className="cart-badge">3</span>
+        </a>
+        <a href="/perfil">
+        <FontAwesomeIcon icon={faUser}/>
+        </a>
+      </div>
+   </div>
+    </header>
+  );
+}
+
+export default Header;
