f863a4b41cc0751658cd7bcc1f126bd16a3df2e3
Murilo358 <murilobarbosa358@gmail.com>
2025-01-02 21:28:51 -0300

    Improved product page

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,19 +1,41 @@
 import { useParams } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
 import config from "../../config";
-import { Typography } from "@mui/material";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Typography,
+} from "@mui/material";
 import ImageZoom from "react-image-zooom";
+import { useState } from "react";
 
 const Product = () => {
+  const [quantity, setQuantity] = useState("1");
+
+  type Category = {
+    id: string;
+    name: string;
+    description: string;
+  };
+
+  type Seller = {
+    name: string;
+    newProductsLastMonth: number;
+    newSalesLastMonth: number;
+  };
+
   type ProductType = {
     id: string;
     name: string;
     description: string;
-    categoryId: string;
+    category: Category;
     createdAt: string;
     inventoryCount: number;
     price: number;
-    sellerId: number;
+    seller: Seller;
     updatedAt: string;
   };
 
@@ -34,6 +56,10 @@ const Product = () => {
   if (error) {
     return <div>Erro: {error}</div>;
   }
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setQuantity(event.target.value as string);
+  };
 
   return (
     <>
@@ -57,15 +83,53 @@ const Product = () => {
                   <div className="flex-grow border-t border-gray-400"></div>
                 </div>
               </div>
-              <div className="w-2/5 flex flex-col  border border-gray-300 p-5 rounded-md gap-4">
-                <Typography variant="h3">{product.name}</Typography>
-                <Typography variant="h4">R${product.price}</Typography>
-                <Typography variant="h6">
-                  {product.inventoryCount} Disponiveis
-                </Typography>
-                <button className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-4 px-4 rounded">
-                  Comprar agora
-                </button>
+              <div className="w-2/5 flex flex-col gap-7">
+                <div className="flex flex-col  border border-gray-300 p-5 rounded-md gap-4">
+                  <Typography variant="h3">{product.name}</Typography>
+                  <Typography variant="h4">R${product.price}</Typography>
+                  <FormControl fullWidth>
+                    <InputLabel id="quantity-select-label">
+                      Quantidade
+                    </InputLabel>
+                    <Select
+                      labelId="quantity-select-label"
+                      value={quantity}
+                      type="number"
+                      label="Quantidade"
+                      onChange={handleChange}
+                    >
+                      {Array.from(
+                        { length: Math.min(product.inventoryCount, 5) },
+                        (_, index) => (
+                          <MenuItem key={index + 1} value={index + 1}>
+                            {index + 1}
+                          </MenuItem>
+                        )
+                      )}
+                      {product.inventoryCount > 5 && (
+                        <MenuItem value="more">Mais de 5 unidades</MenuItem>
+                      )}
+                    </Select>
+                  </FormControl>
+                  <Typography variant="h6">
+                    {product.inventoryCount} Disponiveis
+                  </Typography>
+                  <button className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-4 px-4 rounded">
+                    Comprar agora
+                  </button>
+                </div>
+                <div className="flex flex-col  border border-gray-300 p-5 rounded-md gap-4">
+                  <Typography variant="h5">
+                    Vendido por {product?.seller?.name}
+                  </Typography>
+                  <Typography variant="h6">
+                    + {product?.seller?.newProductsLastMonth} Novos produtos no
+                    último mês
+                  </Typography>
+                  <Typography variant="h6">
+                    + {product?.seller?.newSalesLastMonth} vendas no último mês
+                  </Typography>
+                </div>
               </div>
             </div>
             <div>{product.description}</div>
